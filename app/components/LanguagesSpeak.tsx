1ea81a147826cd978151e735f0a12ee9a18e68d9
ShaYri12 <sharjeelb60@gmail.com>
2024-11-20 21:03:47 +0500

    little styling fix

--- app/components/LanguagesSpeak.tsx
+++ app/components/LanguagesSpeak.tsx
@@ -5,8 +5,8 @@ import FloatingText from "./FloatingText";
 export default function LanguagesSpeak() {
   return (
     <div className="py-[50px] md:py-[70px] overflow-hidden">
-      <div className=" relative z-[999] max-w-[1094.46px] mx-auto">
-        <h1 className="text-4xl md:text-[66px] font-helvetica-bold md:leading-[77px] mb-[30px] md:mb-[45px] text-white text-center">
+      <div className="md:px-8 px-4">
+        <h1 className="relative z-[999] max-w-[1094.46px] mx-auto text-4xl md:text-[66px] font-helvetica-bold md:leading-[77px] mb-[30px] md:mb-[45px] text-white text-center">
           Avatars that speak 32 Languages—Connecting You to a World of
           Possibilities
         </h1>
